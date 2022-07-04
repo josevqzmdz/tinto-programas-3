b475fe48fd56a0fcde2758e4c3b110bd1e507951
Pablo Dávila <pablo.davila95@gmail.com>
2022-07-04 09:14:57 -0500

    latest changes

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,9 +4,9 @@ const csvFilePath = './prisma/data.csv';
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
+// [{columnas_del_excel}] -> Cambia a que las filas vacias no esten vacias, usando el valor de la orden anterior que si exsitia
 const fillEmptyArticles = async (jsonArray) => {
 	const ordersUnparsed = jsonArray.filter((order) => order.article_name);
-
 	let orders = [];
 
 	let order_number;
@@ -56,12 +56,18 @@ const fillEmptyArticles = async (jsonArray) => {
 	return orders;
 };
 
+// [{columnas_del_excel}] -> [{columnas_de_orden: {articles: [{columnas_del_articulo}]]}}}]
 const articlesToOrder = (articles) => {
+	// ['123', '2452', '35653']
 	const order_numbers = Array.from(new Set(articles.map((article) => article.order_number)));
+
+	// {'123': [{columnas_del_excel_filtradas_por_numero_de_orden}], '2452': [{columnas_del_excel_filtradas_por_numero_de_orden}] }
 	const ordered_data = order_numbers.reduce((curr, number) => {
 		curr[number] = articles.filter((article) => article.order_number === number);
 		return curr;
 	}, {});
+
+	// {'123': [{columnas_del_articulo_que_corresponden_a_esa_orden}]}
 	const filtered = order_numbers.reduce((curr, number) => {
 		curr[number] = articles
 			.filter((article) => article.order_number === number)
@@ -76,6 +82,8 @@ const articlesToOrder = (articles) => {
 		return curr;
 	}, {});
 
+
+	// ['123', '252'] -> [{columnas_de_orden: {articles: [{columnas_del_articulo}]]}}}]
 	const orders = order_numbers.map((order_number) => {
 		return {
 			order_number,
@@ -89,6 +97,7 @@ const articlesToOrder = (articles) => {
 			articles: filtered[order_number]
 		};
 	});
+	
 	return orders;
 };
 
@@ -96,6 +105,7 @@ const seed = async () => {
 	const jsonArray = await csv().fromFile(csvFilePath);
 	const articles = await fillEmptyArticles(jsonArray);
 	const orders = articlesToOrder(articles);
+
 	for (const order of orders) {
 		try {
 			await prisma.order.create({ data: { ...order, articles: { create: order.articles } } });
